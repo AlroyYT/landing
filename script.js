89f30a09b16842113f5cbf6d90691d1a59108651
alroy <alroyvader123@gmail.com>
2024-10-02 18:33:43 +0530

    land

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.login-btn').addEventListener('click', function() {
+    window.location.href = '/login'; // Redirect to the login page
+});
+
+document.querySelector('.signup-btn').addEventListener('click', function() {
+    window.location.href = '/signup'; // Redirect to the signup page
+});
